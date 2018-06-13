0683268bbf1b5c8536ecae5e7aea574ab1b3009d
david.huang <david.huang@getac.com.tw>
2018-06-13 20:22:28 +0800

    Add Admin dashboard

--- IrsaWebStore/App_Start/BundleConfig.cs
+++ IrsaWebStore/App_Start/BundleConfig.cs
@@ -25,6 +25,10 @@ namespace IrsaWebStore
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/bootstrap.js"));
         }
     }
 }
