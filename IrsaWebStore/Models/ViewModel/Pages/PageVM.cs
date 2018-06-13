63c260ca00efd749b41067575c9bddeece36c1f4
david.huang <david.huang@getac.com.tw>
2018-06-13 21:13:18 +0800

    Lec 7 done

---
+++ IrsaWebStore/Models/ViewModel/Pages/PageVM.cs
@@ -0,0 +1,36 @@
+﻿using IrsaWebStore.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace IrsaWebStore.Models.ViewModel.Pages
+{
+    public class PageVM
+    {
+        public PageVM()
+        {
+
+        }
+        public PageVM(PageDTO row)
+        {
+            Id = row.Id;
+            Title = row.Title;
+            Slug = row.Slug;
+            Body = row.Body;
+            Sorting = row.Sorting;
+            HasSideBar = row.HasSideBar;
+        }
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength =3)]
+        public string Title { get; set; }
+        public string Slug { get; set; }
+        [Required]
+        [StringLength(int.MaxValue, MinimumLength =3)]
+        public string Body { get; set; }
+        public int Sorting { get; set; }
+        public bool HasSideBar { get; set; }
+    }
+}
