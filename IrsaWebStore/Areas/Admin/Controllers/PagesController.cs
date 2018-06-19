402bbedce14bdccb574e2b1fb1f225eaaa5506fe
david.huang <david.huang@getac.com.tw>
2018-06-19 15:03:50 +0800

    Lec 10 done

--- IrsaWebStore/Areas/Admin/Controllers/PagesController.cs
+++ IrsaWebStore/Areas/Admin/Controllers/PagesController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using IrsaWebStore.Models.Data;
+using IrsaWebStore.Models.ViewModel.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +13,77 @@ namespace IrsaWebStore.Areas.Admin.Controllers
         // GET: Admin/Pages
         public ActionResult Index()
         {
+            // decalse list of pageVM
+            List<PageVM> pagesList;
+
+            // Init the list
+            using (Db db = new Db())
+            {
+                pagesList = db.Pages.ToArray().OrderBy(x => x.Sorting).Select(
+                    x => new PageVM(x)).ToList();
+            }
+
+            // retunr view with list
+            return View(pagesList);
+        }
+
+        // GET : Admin/Pages/AddPage
+        public ActionResult AddPage()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddPage(PageVM model)
+        {
+            //check model state
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+
+            }
+
+            using (Db db = new Db())
+            {
+                // Declare slug
+                string slug;
+                // Init pageDTO
+                PageDTO dto = new PageDTO();
+                // DTO title
+                dto.Title = model.Title;
+                // Check for and set slug if need be
+                if (string.IsNullOrWhiteSpace(model.Slug))
+                {
+                    slug = model.Title.Replace(" ", "-").ToLower();
+                }
+                else
+                {
+                    slug = model.Slug.Replace(" ", "-").ToLower();
+                }
+                // Make sure title and slug are unique
+                if (db.Pages.Any(x => x.Title == model.Title) 
+                    || db.Pages.Any(x => x.Slug == slug))
+                {
+                    ModelState.AddModelError("", "That title or slug already exists.");
+                    return View(model);
+
+                }
+                // DTO reset
+                dto.Slug = slug;
+                dto.Body = model.Body;
+                dto.HasSideBar = model.HasSideBar;
+                dto.Sorting = 100;
+                // Save DTO
+                db.Pages.Add(dto);
+                db.SaveChanges();
+            }
+
+            //Set Tempdata msg
+            TempData["SM"] = "You have added a new page!";
+
+            // Redirect
+
+            return RedirectToAction("AddPage");
         }
     }
 }
--- IrsaWebStore/Models/Data/Db.cs
+++ IrsaWebStore/Models/Data/Db.cs
@@ -8,6 +8,8 @@ namespace IrsaWebStore.Models.Data
 {
     public class Db : DbContext
     {
+        public DbSet<PageDTO> Pages { get; set; }
 
+        public System.Data.Entity.DbSet<IrsaWebStore.Models.ViewModel.Pages.PageVM> PageVMs { get; set; }
     }
 }
