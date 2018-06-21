f54c380ca72781f4ad958e459b2c3e3fbd931527
david.huang <david.huang@getac.com.tw>
2018-06-21 10:58:02 +0800

    Lec 23 done

--- IrsaWebStore/Areas/Admin/Controllers/ShopController.cs
+++ IrsaWebStore/Areas/Admin/Controllers/ShopController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+
 namespace IrsaWebStore.Areas.Admin.Controllers
 {
     public class ShopController : Controller
@@ -25,6 +26,66 @@ namespace IrsaWebStore.Areas.Admin.Controllers
             }
 
             return View(categoryVMList);
+        }
+
+        [HttpPost]
+        public string AddNewCategory(string catName)
+        {
+            string id;
+
+            using (Db db = new Db())
+            {
+                if (db.Categories.Any(x => x.Name == catName))
+                    return "titletaken";
+
+                CategoryDTO dto = new CategoryDTO();
+
+                dto.Name = catName;
+                dto.Slug = catName.Replace(" ", "-").ToLower();
+                dto.Sorting = 100;
+
+                db.Categories.Add(dto);
+                db.SaveChanges();
+
+                id = dto.Id.ToString();
+            }
+            return id;
+        }
+
+        // POST : Admin/Shop/ReorderCategories
+        [HttpPost]
+        public void ReorderCategories(int[] id)
+        {
+            using (Db db = new Db())
+            {
+                int count = 1;
+
+                CategoryDTO dto;
+
+                foreach (var catId in id)
+                {
+                    dto = db.Categories.Find(catId);
+                    dto.Sorting = count;
+
+                    db.SaveChanges();
+                    count++;
+                }
+            }
+        }
+
+        // GET : Admin/Shop/DeleteCategory/id
+        public ActionResult DeleteCategory(int id)
+        {
+            using (Db db = new Db())
+            {
+                CategoryDTO dto = db.Categories.Find(id);
+
+                db.Categories.Remove(dto);
+                db.SaveChanges();
+            }
+
+
+            return RedirectToAction("Categories");
         }
     }
 }
